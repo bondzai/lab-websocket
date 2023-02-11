ec73bfbf7a9672f00f7b13e63f9dfea12ca4e66a
introbond <introbond.dev@gmail.com>
2023-02-11 21:12:28 +0700

    updates: web-socket channel condition

--- backend/ws_api/consumers.py
+++ backend/ws_api/consumers.py
@@ -28,27 +28,44 @@ class MyConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
 
-        location = text_data_json["location"]
-        max_room = text_data_json["max_room"]
-        avaliable_room = text_data_json["avaliable_room"]
-        trigger_count = text_data_json["trigger_count"]
-
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, 
-            {"type": "chat_message", 
-                "location": location,
-                "max_room":max_room,
-                "avaliable_room":avaliable_room,
-            }
-        )
+        if "trigger_count" in text_data_json:
+            print("********** PIR **********")
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {"type": "chat_message",
+                    "location": text_data_json["location"],
+                    "trigger_count": text_data_json["trigger_count"],
+                }
+            )
+        else:
+            print("********** Magnetic **********")
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
+                {"type": "chat_message",
+                    "location": text_data_json["location"],
+                    "max_room": text_data_json["max_room"],
+                    "avaliable_room": text_data_json["avaliable_room"],
+                }
+            )
 
     # Receive message from room group
     def chat_message(self, event):
+        print("**********")
+        print(event)
+        print("**********")
+        
         # Send message to WebSocket
-        self.send(text_data=json.dumps(
-            {
-                "location": event["location"],
-                "max_room": event["max_room"],
-                "avaliable_room": event["avaliable_room"],
-            }))
+        if "trigger_count" in event:
+            self.send(text_data=json.dumps(
+                {
+                    "location": event["location"],
+                    "trigger_count": event["trigger_count"],
+                }))
+        else: 
+            self.send(text_data=json.dumps(
+                {
+                    "location": event["location"],
+                    "max_room": event["max_room"],
+                    "avaliable_room": event["avaliable_room"],
+                }))
