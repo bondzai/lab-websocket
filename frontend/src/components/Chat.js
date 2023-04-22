0706faa9113dff4d9c07cfbd07be786576571e29
JamesBond <introbond.dev@gmail.com>
2023-04-22 13:59:20 +0700

    create sigle cleint config compent

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,65 +1,82 @@
 import React, { useState, useEffect } from 'react';
-import './Chat.css'
+import './Chat.css';
 
 const Chat = () => {
     const [socket, setSocket] = useState(null);
-    const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
     const [isConnected, setIsConnected] = useState(false);
-  
-    useEffect(() => {
-        const newSocket = new WebSocket('ws://localhost:8000/ws/1/');
-        setSocket(newSocket);
-    }, []);
-  
+    const [endpoint, setEndpoint] = useState('wss://aot-dev.sitearound.com/ws/airport/BKK/'); // default endpoint
+
     useEffect(() => {
         if (socket) {
             socket.onopen = () => {
                 console.log('WebSocket connection opened');
                 setIsConnected(true);
             };
-          
+
             socket.onmessage = (event) => {
                 console.log('Received message:', event.data);
-                setMessages([...messages, JSON.parse(event.data)]);
+                setMessages(prevMessages => [JSON.parse(event.data), ...prevMessages]);
             };
-          
+            
+
             socket.onclose = () => {
                 console.log('WebSocket connection closed');
                 setIsConnected(false);
             };
         }
     }, [socket, messages]);
-  
-    const handleMessageChange = (e) => {
-        setMessage(e.target.value);
-    }
 
-    const handleSendClick = () => {
-        socket.send(JSON.stringify({ message: message }));
-        setMessage('');
-    }
+    const handleConnectClick = () => {
+        const newSocket = new WebSocket(endpoint);
+        setSocket(newSocket);
+    };
+
+    const handleDisconnectClick = () => {
+        socket.close();
+        setSocket(null);
+        setIsConnected(false);
+    };
+
+    const handleClearClick = () => {
+        setMessages([]);
+    };
+
+    const connectedStyle = {
+        backgroundColor: isConnected ? 'green' : 'red',
+        display: 'inline-block',
+        borderRadius: '50%',
+        width: '12px',
+        height: '12px',
+        marginRight: '5px',
+        animation: isConnected ? 'blinking 1s infinite' : 'none'
+    };
 
     return (
-        <div>
-            <h1> WebSocket API </h1>
-            <div>
+        <div className='Chat'>
+            <h1> WebSocket Client </h1>
+            <div className='status-panel'>
+                <div style={connectedStyle}></div>
                 {isConnected ? 'Connected' : 'Disconnected'}
             </div>
-            <div>
+            <div className='messages'>
                 <ul>
                     {messages.map((m, index) => (
-                        <li key={index}> {m.message}</li>
+                        <li key={index}>
+                            <pre>{JSON.stringify(m, null, 2)}</pre>
+                        </li>
                     ))}
                 </ul>
             </div>
-            <div>
+            <div className='control-panel'>
                 <input
                     type="text"
-                    value={message}
-                    onChange={handleMessageChange}
+                    value={endpoint}
+                    onChange={(e) => setEndpoint(e.target.value)}
                 />
-                <button onClick={handleSendClick}>Send</button>
+                <button onClick={handleConnectClick}>Connect</button>
+                <button onClick={handleDisconnectClick}>Disconnect</button>
+                <button onClick={handleClearClick}>Clear Data</button>
             </div>
         </div>
     );
